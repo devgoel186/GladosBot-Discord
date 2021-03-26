f4e465eb7d479534a1a9ed463128c98d34f36174
Dev Goel <goeldev01@gmail.com>
2021-03-26 14:48:35 +0530

    Starter commands added

--- index.js
+++ index.js
@@ -3,8 +3,30 @@ require("dotenv").config();
 
 const client = new discord.Client();
 
+const prefix = "$";
+
 client.once("ready", () => {
   console.log("GG! GladosBot is Online!");
+});
+
+client.on("message", (message) => {
+  let msg = message.channel;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  switch (command) {
+    case command === "ping":
+      msg.send("POG!");
+      break;
+    case command === "youtube":
+      msg.send("https://www.youtube.com");
+      break;
+    default:
+      msg.send(`Incorrect command!\nCommand format is : '$command'`);
+      break;
+  }
 });
 
 client.login(process.env.TOKEN);
