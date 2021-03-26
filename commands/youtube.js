bb2e1323e12a53cb2587ec1fe91f8392234bea8e
Dev Goel <goeldev01@gmail.com>
2021-03-26 15:22:51 +0530

    Setup for a larger base of commands

---
+++ commands/youtube.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "youtube",
+  description: "Youtube commands",
+  execute(message, args) {
+    message.channel.send("https://www.youtube.com");
+  },
+};
--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 const discord = require("discord.js");
 require("dotenv").config();
-
 const client = new discord.Client();
-
 const prefix = "$";
+const fs = require("fs");
+
+client.commands = new discord.Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
 client.once("ready", () => {
   console.log("GG! GladosBot is Online!");
 });
 
+let com = client.commands;
 client.on("message", (message) => {
-  let msg = message.channel;
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
   switch (command) {
-    case command === "ping":
-      msg.send("POG!");
+    case "ping":
+      com.get("ping").execute(message, args);
       break;
-    case command === "youtube":
-      msg.send("https://www.youtube.com");
+    case "youtube":
+      com.get("youtube").execute(message, args);
       break;
     default:
-      msg.send(`Incorrect command!\nCommand format is : '$command'`);
+      message.channel.send(
+        `Incorrect command!\nCommand format is : '$command'`
+      );
       break;
   }
 });
